1fd60819988ff8823f11f2e8062bd02cc6825d53
suraju-aishat <163733400+suraju-aishat@users.noreply.github.com>
2024-12-07 13:47:21 +0100

    Update Login.js

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
       dispatch(signInStart())
 
       const res = await axios.post(
-        "http://localhost:3000/api/auth/signin",
+        "https://note-app-backend-xqzt.onrender.com/api/auth/signin",
         { email, password },
         { withCredentials: true }
       )
